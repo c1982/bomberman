b4ab21544855622576ac25b628a46baea1502c80
c1982 <aspsrc@gmail.com>
2018-10-08 23:47:43 +0300

    added haralla gürelle

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # bomberman
-SMTP Load Test
+SMTP Load Test tool 
+
+## Installing
+...
+
+## flags
+
+| Flag        | Desc           | 
+| ------------- |-------------| 
+| host | Remote SMTP server with Port. Example: mail.server.com:25 | 
+| from | From email address | 
+| to | To email address| 
+| subject | Email subject text | 
+| body | Email body text | 
+| helo | SMTP client helo name. Ex: mail.server.com | 
+| count | Email message count |
+| workers | Thread workers for SMTP client. Default: 100 |
+| jobs | Job queue lenght in workers. Default: 50 |
+| outbound | Outbound IP address for SMTP client |
+
+## server configuration checklist
+
+* Check SPF value in domain dns zone
+* Check PTR record your outbound IP address
+
+## usage
+
+Send 100 email to mail.server.com:25 100 workers
+
+```
+./bomberman -host=mail.server.com:25 -from=test@mydomain.com -to=user@remotedomain.com -workers=100 -jobs=50 -count=100 -outbound=YOUR_PUBLIC_IP -helo=mydomain.com -subject="Test Email"
+```
+
+## output
+
+
+```
+Count	: 10
+Error	: 0
+Start	: 2018-10-08 23:07:17.202394064 +0300 EEST m=+0.000830547
+End	: 2018-10-08 23:07:17.792740992 +0300 EEST m=+0.591177492
+Time	: 590.346945ms
+HELO (10)	: min. 183.457938ms, max. 292.501073ms, med. 197.830802ms
+MAIL (10)	: min. 230.36918ms, max. 339.589489ms, med. 246.102343ms
+RCPT (10)	: min. 276.966907ms, max. 386.162844ms, med. 292.913193ms
+DATA (10)	: min. 417.385041ms, max. 542.583022ms, med. 431.915842ms
+SUCCESS (10)	: min. 464.569102ms, max. 589.784965ms, med. 478.931928ms
+DIAL (10)	: min. 8.480266ms, max. 16.660143ms, med. 9.456583ms
+TOUCH (10)	: min. 136.853636ms, max. 245.775096ms, med. 147.872106ms
+```
+
+## Built With
+
+* [grpool](github.com/ivpusic/grpool) - Lightweight Goroutine pool
+* [net/smtp](https://golang.org/pkg/net/smtp/) - Golang SMTP Package
+
+## Author
+
+* **Oğuzhan** - *MaestroPanel Tech Lead* - [c1982](https://github.com/c1982)
--- src/main.go
+++ src/main.go
@@ -3,99 +3,269 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"io"
+	"net"
 	"net/smtp"
+	"runtime"
+	"sort"
 	"time"
+
+	"github.com/ivpusic/grpool"
 )
 
 func main() {
 
+	var errorCount int
+	var totalCount int
+	var metrics []map[string]time.Duration
+
+	numCPUs := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPUs)
+
 	host := flag.String("host", "localhost:25", "-host=example.org:25")
-
-	//TODO: Autogenerate
 	from := flag.String("from", "me@example.org", "-from=me@example.org")
-
-	//TODO: Autogenerate
 	to := flag.String("to", "to@example.net", "-to=me@example.net")
-
-	//TODO: Autogenerate
 	subject := flag.String("subject", "Test Email", "-subject=Test Email")
-
-	//TODO: Autogenerate
-	//TODO: Import from file
 	body := flag.String("body", "Load Test Generator", "-body=Load Test Generator")
 	helo := flag.String("helo", "mail.example.org", "-helo=mail.example.org")
-	concurrent := flag.Int("thread", 10, "-thread=10")
+	count := flag.Int("count", 10, "-count=10")
+	workers := flag.Int("workers", 100, "-workers=100")
+	jobs := flag.Int("jobs", 50, "-jobs=50")
+	outboundip := flag.String("outbound", "127.0.0.1", "-outbound=127.0.0.1")
 
 	flag.Parse()
 
-	ch := make(chan time.Duration)
+	errorCount = 0
+	totalCount = 0
+	metrics = []map[string]time.Duration{}
 
-	for i := 0; i < *concurrent; i++ {
-		go SendMail(*host,
-			*from,
-			*to,
-			*subject,
-			*body,
-			*helo, ch)
-	}
+	pool := grpool.NewPool(*workers, *jobs)
 
-	for i := 0; i < *concurrent; i++ {
-		fmt.Println(<-ch)
-	}
+	defer pool.Release()
 
-}
-
-func SendMail(host, from, to, subject, body, helo string, ch chan<- time.Duration) {
+	pool.WaitCount(*count)
 
 	startTime := time.Now()
 
-	c, err := smtp.Dial(host)
+	for i := 0; i < *count; i++ {
+
+		pool.JobQueue <- func() {
+			totalCount++
+
+			metric, err := SendMail(*outboundip,
+				*host,
+				*from,
+				*to,
+				*subject,
+				*body,
+				*helo)
+
+			if err != nil {
+				fmt.Printf("%d: %v", i, err)
+				errorCount++
+			}
+
+			metrics = append(metrics, metric)
+
+			defer pool.JobDone()
+		}
+	}
+
+	pool.WaitAll()
+	endtime := time.Now()
+
+	fmt.Printf("Count\t: %d\n", totalCount)
+	fmt.Printf("Error\t: %d\n", errorCount)
+	fmt.Printf("Start\t: %v\n", startTime)
+	fmt.Printf("End\t: %v\n", endtime)
+	fmt.Printf("Time\t: %v\n", endtime.Sub(startTime))
+
+	mkeys := metricKeys(metrics)
+
+	for i := 0; i < len(mkeys); i++ {
+		m := mkeys[i]
+		min, max, me := getMetric(m, metrics)
+		cnt := countMetric(m, metrics)
+		fmt.Printf("%s (%d)\t: min. %v, max. %v, med. %v\n", m, cnt, min, max, me)
+	}
+}
+
+//SendMail mail.
+func SendMail(outboundIPaddr, smtpServer, from, to, subject, body, helo string) (metric map[string]time.Duration, err error) {
+	metric = map[string]time.Duration{}
+
+	var wc io.WriteCloser
+	var msg string
+
+	startTime := time.Now()
+
+	dialer := &net.Dialer{Timeout: time.Second * 6}
+	dialer.LocalAddr = &net.TCPAddr{IP: net.ParseIP(outboundIPaddr)}
+
+	host, _, _ := net.SplitHostPort(smtpServer)
+	conn, err := dialer.Dial("tcp", smtpServer)
 
 	if err != nil {
-		log.Println(err)
-		ch <- time.Now().Sub(startTime)
+		err = fmt.Errorf("DIAL: %v", err)
+		metric["DIAL"] = time.Now().Sub(startTime)
+
 		return
 	}
 
+	metric["DIAL"] = time.Now().Sub(startTime)
+
+	c, err := smtp.NewClient(conn, host)
+
+	if err != nil {
+
+		err = fmt.Errorf("TOUCH: %v", err)
+		metric["TOUCH"] = time.Now().Sub(startTime)
+
+		return
+	}
+
+	metric["TOUCH"] = time.Now().Sub(startTime)
 	defer c.Close()
 
-	if err := c.Hello(helo); err != nil {
-		log.Println(err)
-		ch <- time.Now().Sub(startTime)
+	err = c.Hello(helo)
+
+	if err != nil {
+		err = fmt.Errorf("HELO: %v", err)
+		metric["HELO"] = time.Now().Sub(startTime)
+
 		return
 	}
 
-	if err := c.Mail(from); err != nil {
-		log.Println(err)
-		ch <- time.Now().Sub(startTime)
+	metric["HELO"] = time.Now().Sub(startTime)
+
+	err = c.Mail(from)
+
+	if err != nil {
+		err = fmt.Errorf("MAIL: %v", err)
+		metric["MAIL"] = time.Now().Sub(startTime)
+
 		return
 	}
 
-	if err := c.Rcpt(to); err != nil {
-		log.Println(err)
-		ch <- time.Now().Sub(startTime)
+	metric["MAIL"] = time.Now().Sub(startTime)
+
+	err = c.Rcpt(to)
+
+	if err != nil {
+		err = fmt.Errorf("RCPT: %v", err)
+		metric["RCPT"] = time.Now().Sub(startTime)
 		return
 	}
 
-	msg := ""
+	metric["RCPT"] = time.Now().Sub(startTime)
+
+	msg = ""
+	msg += fmt.Sprintf("from: <%s>\r\n", from)
+	msg += fmt.Sprintf("to: %s\r\n", to)
 	msg += fmt.Sprintf("Subject: %s\r\n", subject)
 	msg += fmt.Sprintf("\r\n%s", body)
 
-	wc, err := c.Data()
+	wc, err = c.Data()
+
+	if err != nil {
+		err = fmt.Errorf("DATA: %v", err)
+		metric["DATA"] = time.Now().Sub(startTime)
+		return
+	}
+
 	_, err = fmt.Fprintf(wc, msg)
-	_ = wc.Close()
+
+	err = wc.Close()
+
+	if err != nil {
+		err = fmt.Errorf("DATA: %v", err)
+		metric["DATA"] = time.Now().Sub(startTime)
+		return
+	}
+
+	metric["DATA"] = time.Now().Sub(startTime)
 
 	err = c.Quit()
 
 	if err != nil {
-		log.Println(err)
-		ch <- time.Now().Sub(startTime)
+		err = fmt.Errorf("QUIT: %v", err)
+		metric["QUIT"] = time.Now().Sub(startTime)
 		return
 	}
 
-	endTime := time.Now()
-	elapsed := endTime.Sub(startTime)
+	metric["SUCCESS"] = time.Now().Sub(startTime)
 
-	ch <- elapsed
+	return
+}
+
+func getMetric(name string, metrics []map[string]time.Duration) (max, min, med time.Duration) {
+
+	totaldur, _ := time.ParseDuration("0ms")
+	list := []time.Duration{}
+
+	for i := 0; i < len(metrics); i++ {
+		m := metrics[i]
+
+		if t, ok := m[name]; ok {
+			totaldur += t
+			list = append(list, t)
+		}
+	}
+
+	sort.Slice(list, func(i, j int) bool {
+		return list[i] > list[j]
+	})
+
+	min = list[0]
+	max = list[len(list)-1]
+	med = totaldur / time.Duration(len(list))
+
+	return
+}
+
+func countMetric(name string, metrics []map[string]time.Duration) (cnt int) {
+
+	for i := 0; i < len(metrics); i++ {
+		m := metrics[i]
+
+		for mkey := range m {
+
+			if mkey == name {
+				cnt++
+			}
+		}
+	}
+
+	return
+}
+
+func metricKeys(metrics []map[string]time.Duration) (keys []string) {
+
+	for i := 0; i < len(metrics); i++ {
+		m := metrics[i]
+
+		for mkey := range m {
+			contain := isContain(mkey, keys)
+
+			if !contain {
+				keys = append(keys, mkey)
+			}
+		}
+	}
+
+	return
+}
+
+func isContain(key string, keys []string) bool {
+
+	exists := false
+
+	for z := 0; z < len(keys); z++ {
+		if keys[z] == key {
+			exists = true
+			break
+		}
+	}
+
+	return exists
 }
